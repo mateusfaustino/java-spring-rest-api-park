09798d989a061fc8ab06988ee9c5cfe44787f6f5
mateus <mateusfaustino2024@gmail.com>
2025-07-21 19:40:33 -0300

    spring config

---
+++ src/main/java/br/com/mateusfaustino/java_spring_rest_api_park/JavaSpringRestApiParkApplication.java
@@ -0,0 +1,13 @@
+package br.com.mateusfaustino.java_spring_rest_api_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRestApiParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringRestApiParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mateusfaustino/java_spring_rest_api_park/JavaSpringRestApiParkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mateusfaustino.java_spring_rest_api_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringRestApiParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
