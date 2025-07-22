ba347cdc5d7e5465703a8517bb1b7974847bd1f1
mateus <mateusfaustino2024@gmail.com>
2025-07-21 21:03:04 -0300

    Entity User

---
+++ src/main/java/br/com/mateusfaustino/java_spring_rest_api_park/entity/User.java
@@ -0,0 +1,68 @@
+package br.com.mateusfaustino.java_spring_rest_api_park.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name="users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue
+
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String userName;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_by")
+
+    private String createdBy;
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    public enum Role{
+        ROLE_ADMIN,
+        ROLE_CUSTOMER
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
